3d4a247560551c24387e776aa99202b6fdfb6a30
River Magnetic <magneticcontinuum@gmail.com>
2018-01-21 02:58:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,12 +20,3 @@ function deepestChild(){
   
 }
 
-/*function deepestChild() {
-  let node = document.getElementById('grand-node')
-  let nextNode = node.children[0]
-
-  while (nextNode) {
-    node = nextNode
-    nextNode = node.children[0]
-  }
-}*/
